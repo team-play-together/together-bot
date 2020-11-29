8ce839c6bf46d0be9a47e170fe28773f897b089a
wraithkim <wraithtorch@gmail.com>
2020-11-29 13:09:00 +0900

    add attributes brief and help to all commands
    - 개발되지 않은 명령어는 brief=TBD로 통일함.
    - help 명령으로 접근할 때 출력되는 순서에 맞춰서 brief->help 순으로
    통일함.

--- together_bot/bot.py
+++ together_bot/bot.py
@@ -47,26 +47,36 @@ async def on_message(message: discord.Message):
     await bot.process_commands(message)
 
 
-@commands.command(name="random")
+@commands.command(
+    name="random",
+    brief="1부터 주어진 수 사이의 임의의 자연수를 출력함.",
+    help="num에 1 이상의 자연수를 넣으면, 1부터 num 사이의 임의의 값을 출력함.\n(그러니까 0 이하의 수나 자연수가 아닌 걸 넣으면 로그 따서 널 XXXXX....)",
+)
 async def _random(ctx, num: int):
     random_number = random.randrange(num) + 1
     await ctx.send(f"Random number : {random_number}")
 
 
-@commands.command()
+@commands.command(
+    brief="together-bot의 repo url을 출력함.",
+    help="아니 이 명령어를 help까지 쳐본다고? 그런 당신에게는 role get bot developer",
+)
 async def repo(ctx):
     await ctx.send(
         "Bot repository URL : https://github.com/team-play-together/together-bot"
     )
 
 
-@commands.command()
+@commands.command(brief="구글 검색 결과의 url을 출력함.", help="args에 검색할 내용을 넣으면 됨.")
 async def google(ctx, *args):
     query = "+".join(args)
     await ctx.send(f"https://www.google.com/search?q={query}")
 
 
-@commands.command()
+@commands.command(
+    brief="봇이 탁구를 쳐줍니다. 실행 중이라면 무조건 받아칩니다.",
+    help="봇이 실행 중이라면 pong을 출력함.\n(근데 봇이 죽어있으면 help도 안 나오는데 핑을 설명해 줘야해?)",
+)
 async def ping(ctx):
     await ctx.send("pong!")
 
