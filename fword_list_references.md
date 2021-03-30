0c684def6062fdc8c20b3145a8e083c7a3eef9ac
wraithkim <wraithtorch@gmail.com>
2021-03-30 13:46:49 +0900

    fword 명령어 추가
    - 자신의 메세지에 대해 비속어 탐지 ON/OFF 기능
    - 비속어 리스트와 가공 도구 추가
    - 메세지를 모니터링 하고 비속어 감지시 메세지를 삭제하고 필터링 된
    메세지를 보내는 기능

---
+++ fword_list_references.md
@@ -0,0 +1,7 @@
+# 출처
+
+* [한국의 욕설 사전 리스트 by 기서무나구물](http://statwith.com/%ED%95%9C%EA%B5%AD%EC%9D%98-%EC%9A%95%EC%84%A4-%EC%82%AC%EC%A0%84-%EB%A6%AC%EC%8A%A4%ED%8A%B8/)
+* [인스티즈 욕설 및 금지어 리스트](https://www.instiz.net/notice/199)
+* [organization/Gentleman](https://github.com/organization/Gentleman)
+* [욕 필터링](https://asus1004.tistory.com/241)
+* [생활코딩 - 한국 욕 관련 글](https://m.facebook.com/groups/174499879257223?view=permalink&id=2681407568566429)
--- together_bot/bot.py
+++ together_bot/bot.py
@@ -13,6 +13,7 @@ import together_bot.commands
 import together_bot.role
 import together_bot.time
 import together_bot.weather
+import together_bot.fword
 
 ROOT_DIR = Path(__file__).parent.parent
 CONFIG_PATH = os.path.join(ROOT_DIR, "logging.yml")
@@ -54,6 +55,7 @@ def setup(bot: commands.Bot):
     together_bot.role.setup(bot)
     together_bot.time.setup(bot)
     together_bot.weather.setup(bot)
+    together_bot.fword.setup(bot)
 
 
 def start():
