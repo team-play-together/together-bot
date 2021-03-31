666815972ff64fc2cdd47f137274c7a62d5c5477
wraithkim <wraithtorch@gmail.com>
2021-03-31 16:00:42 +0900

    현재 시간을 NTP 서버를 통해 가져옴, time convert 출력 오류 수정
    * time convert가 입력받은 시간을 UTC로 간주하는 오류 해결

--- together_bot/time.py
+++ together_bot/time.py
@@ -1,16 +1,19 @@
 import logging
 from datetime import datetime, timedelta, timezone
 
+import ntplib
 from dateutil.parser import parse
 from discord.ext import commands
 
 tz_pst = timezone(-timedelta(hours=8))
 tz_kst = timezone(timedelta(hours=9))
+TIME_SERVER = "pool.ntp.org"
 
 
 class Time(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
+        self.ntp_client = ntplib.NTPClient()
 
     @commands.group(brief="시간 관련 명령어 모음")
     async def time(self, ctx: commands.Context):
@@ -20,10 +23,17 @@ class Time(commands.Cog):
 
     @time.command(brief="현재 시간을 UTC와 PST로 보여줌.")
     async def now(self, ctx: commands.Context):
-        dt = datetime.now(timezone.utc)
-        await ctx.send(
-            f"UTC: {dt.isoformat(' ', 'seconds')}\nPST: {dt.astimezone(tz_pst).isoformat(' ', 'seconds')}"
-        )
+        try:
+            response = self.ntp_client.request(TIME_SERVER, version=3)
+            dt = datetime.fromtimestamp(response.orig_time, timezone.utc)
+            await ctx.send(
+                f"UTC: {dt.isoformat(' ', 'seconds')}\n"
+                + f"PST: {dt.astimezone(tz_pst).isoformat(' ', 'seconds')}\n"
+                + f"이 정보는 `{TIME_SERVER}` 로부터 제공받았습니다."
+            )
+        except ntplib.NTPException:
+            logging.error("NTP server doesn't respond")
+            await ctx.send("NTP 서버에 연결할 수 없습니다. 나중에 다시 시도해주세요.")
 
     @time.command(
         brief="입력된 한국 시간을 UTC와 PST로 변환해서 보여줌.",
@@ -31,9 +41,10 @@ class Time(commands.Cog):
     )
     async def convert(self, ctx: commands.Context, *, kst_time: str):
         try:
-            dt = parse(kst_time).astimezone(tz_kst)
+            dt = parse(kst_time, ignoretz=True).astimezone(tz_kst)
             await ctx.send(
-                f"UTC: {dt.astimezone(timezone.utc).isoformat(' ', 'seconds')}\nPST: {dt.astimezone(tz_pst).isoformat(' ', 'seconds')}"
+                f"UTC: {dt.astimezone(timezone.utc).isoformat(' ', 'seconds')}\n"
+                + f"PST: {dt.astimezone(tz_pst).isoformat(' ', 'seconds')}"
             )
         except ValueError:
             logging.error("unknown string format: " + kst_time)
